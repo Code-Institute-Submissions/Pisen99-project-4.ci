86be87e359078132762883f361087a3986e1167f
Pisen99 <patricia.andersson99@hotmail.com>
2023-04-03 16:30:59 +0000

    Feat: added templates from prev project. Fixed: updated bookings/urls.py & views.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index_view
+from .views import index_view, signup_view, login_view, resetpassword_view, contact_view, bookings_view, menu_view
+
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('signup', signup_view, name='signup'),
+    path('login', login_view, name='login'),
+    path('resetpassword', resetpassword_view, name='resetpassword'),
+    path('contact', contact_view, name='contact'),
+    path('bookings', bookings_view, name='bookings'),
+    path('menu', menu_view, name='menu'),
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -5,3 +5,33 @@ from django.shortcuts import render
 def index_view(request):
 
     return render(request, 'index.html')
+
+
+def signup_view(request):
+
+    return render(request, 'signup.html')
+
+
+def login_view(request):
+
+    return render(request, 'login.html')
+
+
+def resetpassword_view(request):
+
+    return render(request, 'resetpassword.html')
+
+
+def contact_view(request):
+
+    return render(request, 'contact.html')
+
+
+def bookings_view(request):
+
+    return render(request, 'bookings.html')
+
+
+def menu_view(request):
+
+    return render(request, 'menu.html')
